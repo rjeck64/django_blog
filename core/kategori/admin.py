38bda08bc08d4244bff94dc68e44baad3c6b9ee0
Reyhan <45694726+rjeck64@users.noreply.github.com>
2023-10-24 21:16:17 +0700

    Delete core directory

--- core/kategori/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from kategori.models import Category
-
-# Register your models here.
-admin.site.register(Category)
--- core/login_admin/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_admin , name="login"),
-    path('register/', views.register , name="register"),
-    path('logout/' , views.logout_user , name = 'logout')
-]
